7d1d03762aab7dbb45bae125a33dbc89b5cc88b2
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-10-30 10:58:39 -0500

    login works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,20 @@
 import firebase from 'firebase';
 
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/myNavBar/myNavBar';
+
 import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
-import 'bootstrap';
+
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  console.error('hi', firebase.apps);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavbar.logoutEvent();
+  // console.error('hi', firebase.apps);
 };
 
 init();
